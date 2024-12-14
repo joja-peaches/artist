ad5771fcd32b6afd0ec2e968e0c1444b521d52fd
unknown <joja.peaches7@gmail.com>
2024-12-14 00:24:38 -0600

    Refine commercial nav bar transitions

--- scripts/commercial.js
+++ scripts/commercial.js
@@ -1,24 +1,24 @@
 // Commerical page
 
-const igButton = document.querySelector(".comm-nav__social-icon-ig");
-const linkedinButton = document.querySelector(".comm-nav__social-icon-linkedin");
-const redIgButton = './images/icons/red-instagram-logo.png';
-const whiteIgButton = './images/icons/white-instagram-logo.png';
-const redLinkedinButton = './images/icons/red-linkedin-logo.png';
-const whiteLinkedinButton = './images/icons/white-linkedin-logo.png';
+// const igButton = document.querySelector(".comm-nav__social-icon-ig");
+// const linkedinButton = document.querySelector(".comm-nav__social-icon-linkedin");
+// const redIgButton = './images/icons/red-instagram-logo.png';
+// const whiteIgButton = './images/icons/white-instagram-logo.png';
+// const redLinkedinButton = './images/icons/red-linkedin-logo.png';
+// const whiteLinkedinButton = './images/icons/white-linkedin-logo.png';
 
-igButton.addEventListener("mouseover", () => {
-  igButton.src = redIgButton;
-});
+// igButton.addEventListener("mouseover", () => {
+//   igButton.src = redIgButton;
+// });
 
-igButton.addEventListener("mouseout", () => {
-  igButton.src = whiteIgButton;
-});
+// igButton.addEventListener("mouseout", () => {
+//   igButton.src = whiteIgButton;
+// });
 
-linkedinButton.addEventListener("mouseover", () => {
-  linkedinButton.src = redLinkedinButton;
-});
+// linkedinButton.addEventListener("mouseover", () => {
+//   linkedinButton.src = redLinkedinButton;
+// });
 
-linkedinButton.addEventListener("mouseout", () => {
-  linkedinButton.src = whiteLinkedinButton;
-});
+// linkedinButton.addEventListener("mouseout", () => {
+//   linkedinButton.src = whiteLinkedinButton;
+// });
